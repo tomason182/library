cfcd81f0438ac9d9ff3da1fa5c7213b533a87d4a
tomason182 <elotro182@hotmail.com>
2024-03-14 20:09:00 -0300

    create getBookInfo function. Need to figure out  insertBook

--- scripts.js
+++ scripts.js
@@ -1,4 +1,26 @@
-const addBook = document.getElementById("add-book-button")
-addBook.addEventListener('click', () => {
+const books = document.getElementById("add-book-button");
+const table = document.getElementById("book-table");
+
+
+
+function getBookInfo() {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const status = document.getElementById("status").value;
+
+    return {title, author, pages, status};
+}
+
+function insertBook() {
+
+    //see how to insert rows in table with return of getBookInfo
+    tableBody = table.getElementsByTagName("tbody");
+    document.createElement("td")
+ 
+ }
+
+
+books.addEventListener('click', () => {
     bookEntry.showModal();
-    });
+    })
