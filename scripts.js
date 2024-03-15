3d086b72bcaa897ca14b2b8403d2488c6f6cf0ae
tomason182 <elotro182@hotmail.com>
2024-03-15 11:31:06 -0300

    create insertBook function

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ function getBookInfo() {
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
-    const status = document.getElementById("status").value;
+    const readStatus = document.getElementById("status").value;
 
     return {title, author, pages, status};
 }
@@ -15,10 +15,22 @@ function getBookInfo() {
 function insertBook() {
 
     //see how to insert rows in table with return of getBookInfo
-    tableBody = table.getElementsByTagName("tbody");
-    const newTd = document.createElement("td");
-    
- 
+    const tableBody = document.getElementById("book-table").getElementsByTagName("tbody");
+    const tableBodyElement = tableBody[0];
+    const newRow = tableBodyElement.insertRow();
+    const titleCell = newRow.insertCell(0);
+    const authorCell = newRow.insertCell(1);
+    const pagesCell = newRow.insertCell(2);
+    const statusCell = newRow.insertCell(3);
+
+    const addTitle = document.createTextNode(title);
+    const addAuthor = document.createTextNode(author);
+    const addPages = document.createTextNode(pages);
+    const addStatus = document.createTextNode(readStatus);
+    titleCell.appendChild(addTitle);
+    authorCell.appendChild(addAuthor);
+    pagesCell.appendChild(addPages);
+    statusCell.appendChild(addStatus);
  }
 
 
